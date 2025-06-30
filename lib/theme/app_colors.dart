ad9d6e0429d80b4f47cdf50cb165341d4a7ce7e8
Adrian Molina <adrianmove123@gmail.com>
2025-06-30 00:21:09 -0500

    Enhances UI with updated theme and splash screen
    Refines app design by introducing a new color palette and Glassmorphism styling. Updates splash screen background and icon colors to improve visual consistency across platforms.
    
    Adds `flutter_animate` dependency for animations and integrates animated transitions in the login screen. Customizes theme elements such as buttons, text fields, and overall app styling for a modern, cohesive look.
    
    Fixes minor formatting issues and improves code readability.

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color color1 = Color(0xFF74A3E4);
+  static const Color color2 = Color(0xFF1886E4);
+  static const Color color3 = Color(0xFF053B99);
+  static const Color color4 = Color(0xFF11D6F8);
+  static const Color color5 = Color(0xFFE3EEF9);
+  static const Color color6 = Color(0xFF588EE2);
+  static const Color color7 = Color(0xFF17B8F2);
+  static const Color color8 = Color(0xFF075DCE);
+  static const Color color9 = Color(0xFF12599F);
+  static const Color color10 = Color(0xFF9CB4EC);
+}
